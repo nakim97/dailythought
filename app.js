029b52ad0d597cf5dd8eeebc4e102791fb810873
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-04-14 22:26:57 -0400

    error template and error handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
 const Blog = require('./models/blog');
 
@@ -24,56 +25,76 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
 })
- 
-app.get('/blogs',catchAsync( async(req,res) => {
+
+app.get('/blogs', catchAsync(async (req, res) => {
     const blogs = await Blog.find({});
-    res.render('blogs/index',{blogs})
+    res.render('blogs/index', {
+        blogs
+    })
 }))
 
-app.get('/blogs/new',(req,res) => {
+app.get('/blogs/new', (req, res) => {
     res.render('blogs/new');
 })
 
-app.post('/blogs', catchAsync( async (req,res,next) => {
+app.post('/blogs', catchAsync(async (req, res, next) => {
+    if (!req.body.blog) throw new ExpressError('Invalid', 400);
     const blog = new Blog(req.body.blog);
     await blog.save();
     res.redirect(`/blogs/${blog._id}`);
-    
+
 }))
 
-app.get('/blogs/:id', catchAsync(async (req,res) => {
+app.get('/blogs/:id', catchAsync(async (req, res) => {
     const blog = await Blog.findById(req.params.id)
-    res.render('blogs/show', {blog});
+    res.render('blogs/show', {
+        blog
+    });
 }))
 
 app.get('/blogs/:id/edit', catchAsync(async (req, res) => {
     const blog = await Blog.findById(req.params.id)
-    res.render('blogs/edit', { blog });
+    res.render('blogs/edit', {
+        blog
+    });
 }))
- 
-app.put('/blogs/:id',catchAsync( async (req, res) => {
-    const { id } = req.params;
-    const blog = await Blog.findByIdAndUpdate(id, { ...req.body.blog });
+
+app.put('/blogs/:id', catchAsync(async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const blog = await Blog.findByIdAndUpdate(id, {
+        ...req.body.blog
+    });
     res.redirect(`/blogs/${blog._id}`)
 }));
 
 app.delete('/blogs/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
+    const {
+        id
+    } = req.params;
     await Blog.findByIdAndDelete(id);
     res.redirect('/blogs');
 }))
 
-app.use((err,req,res,next)=> {
-    res.send('UH OH...')
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
     console.log('Serving on port 3000!!!')
 })
- 
